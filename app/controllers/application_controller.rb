f8e45d68b2a1a3d23eb738dd6bf9a39fd1182562
kawabata <s1931115@s.konan-u.ac.jp>
2021-12-27 17:45:49 +0900

    nameを追加できるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+        before_action :configure_permitted_parameters, if: :devise_contlloer?
+        def configure_permitted_parameters
+                devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        end
 end
