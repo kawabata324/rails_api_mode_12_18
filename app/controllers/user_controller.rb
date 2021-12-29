5b55d7f1a76701276484172bf39a776ef6d9fd35
kawabata <s1931115@s.konan-u.ac.jp>
2021-12-29 18:30:28 +0900

    nameの変更を可能にした

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,4 +5,18 @@ class UserController < ApplicationController
   def show
     render json: { user: current_v1_user}
   end
+
+  def name
+    @user = User.find_by(id: current_v1_user.id)
+    @user.name = name_params
+    if @user.save
+      render json: {status: 'ok'}
+    else
+      render json: {status: 'no'}
+    end
+  end
+
+  def name_params
+    params.require(:user).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :hello
   get 'users/show', to: "user#show"
+  post 'users/name', to: "user#name"
 end
